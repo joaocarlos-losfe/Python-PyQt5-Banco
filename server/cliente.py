18cae169236b374d0f1f8905f1a2104acffc19ea
joaocarlos-losfe <joaocarlos.losfe@gmail.com>
2021-10-26 19:55:18 -0300

    prototipo base do cliente servidor finalizado

---
+++ server/cliente.py
@@ -0,0 +1,35 @@
+import socket
+
+class Client:
+        
+    @staticmethod
+    def enviar_dados(mensagem:str):
+        try:
+            print("---cliente---")
+
+            host = "localhost"
+            porta = 8000
+            endereco = (host, porta)
+
+            cliente_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            cliente_socket.connect(endereco)
+
+            mensagem_cliente = ''
+            mensagem_servidor = ''
+
+            mensagem_cliente = mensagem
+            cliente_socket.send(mensagem_cliente.encode())
+
+            mensagem_servidor = cliente_socket.recv(1024)
+            mensagem_servidor = mensagem_servidor.decode()
+            print(f"server response: {mensagem_servidor}")
+
+            cliente_socket.close()
+            print("dados enviados")
+            print("conexao encerrada...")
+                
+        except:
+            print("servidor nao esta em execucao...")
+
+
+Client.enviar_dados("cadastro/joao/sousa/111/333")
